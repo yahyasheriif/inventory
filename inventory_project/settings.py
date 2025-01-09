ea6179fe6d25a0e9334603b48febd6f937d779b4
yahyasheriif <149334812+yahyasheriif@users.noreply.github.com>
2025-01-09 15:02:48 +0200

    Update settings.py

--- inventory_project/settings.py
+++ inventory_project/settings.py
@@ -127,13 +127,18 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'statics'
+MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
